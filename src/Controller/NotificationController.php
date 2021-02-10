925e7761264348f72cbf210054db2208f09b9a2b
flore <florent.duthoitgmail.com>
2021-02-10 21:52:08 +0100

    Add new type of notifications

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -6,15 +6,18 @@ namespace FMDD\SyliusMarketingPlugin\Controller;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\Join;
+use FMDD\SyliusMarketingPlugin\Entity\InstagramPost;
 use FMDD\SyliusMarketingPlugin\Entity\Notification;
 use FMDD\SyliusMarketingPlugin\Entity\NotificationUser;
 use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Sylius\Component\Core\Model\ShopUser;
 use Sylius\Component\Product\Model\Product;
+use Sylius\Component\Product\Model\ProductInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
 
 class NotificationController extends AbstractController
 {
@@ -26,13 +29,19 @@ class NotificationController extends AbstractController
      * @var DateTimeFormatter
      */
     private DateTimeFormatter $dateTimeFormatter;
+    /**
+     * @var EngineInterface
+     */
+    private EngineInterface $templating;
 
     public function __construct(
         Registry $doctrine,
-        DateTimeFormatter $dateTimeFormatter
+        DateTimeFormatter $dateTimeFormatter,
+        EngineInterface $templating
     ) {
         $this->doctrine = $doctrine;
         $this->dateTimeFormatter = $dateTimeFormatter;
+        $this->templating = $templating;
     }
 
     public function randomAction(Request $request)
@@ -66,12 +75,12 @@ class NotificationController extends AbstractController
                         $em->persist($notificationUser);
                         $em->flush();
 
+                        $html = $this->generateNotificationTemplate($notification);
+
                         return new JsonResponse([
                             'error' => false,
                             'notification' => [
-                                'type' => $notification->getType()->getCode(),
-                                'options' => $notification->getOptions(),
-                                'created_at' => $this->dateTimeFormatter->formatDiff($notification->getCreatedAt(), new \DateTime()),
+                                'template' => $html
                             ],
                         ]);
                     }
@@ -87,5 +96,34 @@ class NotificationController extends AbstractController
             }
         }
         return new Response('', 404);
+    }
+
+    /**
+     * TODO: Move to a tagged service, easier to add custom notification
+     * @param Notification $notification
+     * @return string
+     */
+    private function generateNotificationTemplate(Notification $notification) {
+        switch($notification->getType()->getCode()) {
+            case 'purchase':
+                $product = $this->getDoctrine()->getRepository(ProductInterface::class)->find($notification->getOptions()['product_id']);
+                if(is_null($product))
+                    return '';
+                return $this->templating->render('@FMDDSyliusMarketingPlugin/Notification/Type/_purchase.html.twig', [
+                    'product' => $product,
+                    'quantity' => $notification->getOptions()['quantity'],
+                    'firstname' => $notification->getOptions()['firstname'],
+                    'city' => $notification->getOptions()['city'],
+                    'country' => $notification->getOptions()['country'],
+                    'created_at' => $this->dateTimeFormatter->formatDiff($notification->getCreatedAt(), new \DateTime()),
+                ]);
+            case 'trustpilot':
+                return $this->templating->render('@FMDDSyliusMarketingPlugin/Notification/Type/_trustpilot.html.twig', $notification->getOptions());
+            case 'instagram':
+                $posts = $this->getDoctrine()->getRepository(InstagramPost::class)->findAll();
+                shuffle($posts);
+
+                return $this->templating->render('@FMDDSyliusMarketingPlugin/Notification/Type/_instagram.html.twig', ['post' => $posts[0]]);
+        }
     }
 }
